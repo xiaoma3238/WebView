a675fcc544f5d6ad31539dc51956f82872cd67a8
xiaoma3238 <1476319275@qq.com>
2015-07-25 09:08:31 +0800

    second commit

---
+++ Users/lanou3g/Desktop/cocopds1/Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// GCJSONKit
+#define COCOAPODS_POD_AVAILABLE_GCJSONKit
+#define COCOAPODS_VERSION_MAJOR_GCJSONKit 1
+#define COCOAPODS_VERSION_MINOR_GCJSONKit 5
+#define COCOAPODS_VERSION_PATCH_GCJSONKit 0
+
