0749e7f09d88193bec1406c1d2acfb6686f91d84
xiaoma3238 <1476319275@qq.com>
2015-07-25 09:14:23 +0800

    second commit

--- Users/lanou3g/Desktop/cocopds1/cocopds1/AppDelegate.m
+++ Users/lanou3g/Desktop/cocopds1/cocopds1/AppDelegate.m
@@ -19,8 +19,20 @@
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    
+    
+    
     return YES;
 }
+
+
+-(void)printf
+{
+    
+}
+
+
+
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
